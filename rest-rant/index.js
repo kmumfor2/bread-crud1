a15925f7949b5e05b5950583f07a1dba65331556
kmumfor2 <120679405+kmumfor2@users.noreply.github.com>
2023-05-04 17:32:19 -0400

    Adding places controller

--- rest-rant/index.js
+++ rest-rant/index.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.use('/places', require('./controllers/places'))
+
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
@@ -11,4 +13,5 @@ app.get('*', (req, res) => {
 })
 
 app.listen(process.env.PORT)
+
 
